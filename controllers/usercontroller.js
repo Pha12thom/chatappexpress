64e5819dcbc758816f7d848c6451dd4eb91c697a
Milugo Geofrey <122834673+Pha12thom@users.noreply.github.com>
2024-11-19 22:19:14 +0300

    add register and login controllers

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,7 +1,16 @@
-const UserModel = require('../models/usermodel');
+const UserModel = require('../models/userModel');
+const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const validator = require('validator');
+const dotenv = require('dotenv');
+const app = express();
+
+
+app.use(express.json());
+
+
+dotenv.config();
 
 const createToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -10,23 +19,26 @@ const createToken = (id) => {
 }
 
 const register = async(req, res) => {
+    console.log(req.body); // Log the body to see what's being received
     const { name, email, password } = req.body;
+
+    if (!email || !password || !name) {
+        return res.status(400).json('Please fill all fields');
+    }
+
     let user = await UserModel.findOne({ email });
     if (user) return res.status(400).json('User already exists');
-    if (!email || !password || !name) return res.status(400).json('Please fill all fields');
     if (!validator.isEmail(email)) return res.status(400).json('Invalid email');
     if (!validator.isStrongPassword(password)) return res.status(400).json('Password is weak');
-    user = new UserModel({
-        name,
-        email,
-        password
-    });
+
+    user = new UserModel({ name, email, password });
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(password, salt);
     await user.save();
     const token = createToken(user._id);
     res.status(201).json({ token, user });
-}
+};
+
 
 
 const login = async(req, res) => {
